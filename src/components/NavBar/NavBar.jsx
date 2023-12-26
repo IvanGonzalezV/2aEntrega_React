d5f06a9e16b8d7c9a090d18467cef739b035dfc8
Ivan Gonzalez Vargas <ronin7_@hotmail.com>
2023-12-26 11:00:52 -0600

    add_twelve

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,15 @@
 import "./NavBar.css";
 import CartWidget from "../CartWidget/CartWidget";
 
+import { Link } from "react-router-dom"
 
 const NavBar = () => {
     return (
         <nav className="NavBar">
             <ul className="nav-ul">
-                <li className="li">Home</li>
-                <li className="li">Categories</li>
-                <li className="li">Contact</li>
+                <li className="li"><Link to="/">Home</Link></li>
+                <li className="li"><Link to="/Categories">Categories</Link></li>
+                <li className="li"><Link to="/Contact">Contact</Link></li>
                 <li className="Nav"><CartWidget /></li>
             </ul>
         </nav>
